ba4fb595aa2934aaec6abf6c624673ace3dc7fe9
Malin Olsson <miragia.solinde@gmail.com>
2022-04-29 13:56:37 +0200

    Smaller changes and added the help function

--- swapiinfo/src/pages/FilmsPage.jsx
+++ swapiinfo/src/pages/FilmsPage.jsx
@@ -1,21 +1,22 @@
 import SwAPI from '../services/SwAPI'
 import { useEffect, useState } from 'react'
-import CardsFilmItem from '../components/CardsFilmItem'
+import CardsFilm from '../components/CardsFilmItem'
 
 
 const FilmsPage = () => {
-    const [films, setFilms] = useState([])
+    const [films, setFilms] = useState()
+    const [page, setPage] = useState(1)
 
     //Get films from api
     const getFilms = async () => {
-        const data = await SwAPI.getFilms()
+        const data = await SwAPI.getFilms(page)
         setFilms(data)
 
 
         //Get films from api when component is first mounted
         useEffect(() => {
             getFilms
-        },[])
+        },[page])
     }
 
     return (
@@ -24,7 +25,7 @@ const FilmsPage = () => {
             <h1>Star Wars Films</h1>
 
             <div className='mb-3'>
-                < CardsFilmItem />
+                < CardsFilm />
             </div>
 
         </div>
